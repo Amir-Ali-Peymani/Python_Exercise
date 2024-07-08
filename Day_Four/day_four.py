4f1f7bec42596338839af55057eb72e84c423b40
amrialiPeymani <piliconwalys@gmail.com>
2024-07-08 20:36:26 +0330

    fnish day four

---
+++ Day_Four/day_four.py
@@ -0,0 +1,7 @@
+import random
+
+names = ["amir ali", "cc-4477", "ct-7567", "cc-2224", "ct-5385"]
+
+index_number = random.randint(0, len(names)+1)
+
+print("this person: "+names[index_number]+" is going to pay!!")
