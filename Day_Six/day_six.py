70a7ccd65c0aba867df3268d81f7faa5f72650a7
amrialiPeymani <piliconwalys@gmail.com>
2024-07-09 00:25:55 +0330

    finish Day six

---
+++ Day_Six/day_six.py
@@ -0,0 +1,8 @@
+#this day was about learning while 
+#i knew about them so i write my own 
+#simple programm
+
+number = int(input("how many times? "))
+while(number>0):
+    print("hello there")
+    number -= 1
