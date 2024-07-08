95529ab80befde1f8dc408a563df571df976700d
amrialiPeymani <piliconwalys@gmail.com>
2024-07-08 19:25:02 +0330

    finish day one

---
+++ Day_One/day_one.py
@@ -0,0 +1,4 @@
+print("hello there welcome, please write down you name & last name")
+name = input("what is your name? \n")
+last_name = input("what is your last name? \n")
+print("oh nice to meet you "+ name+ " "+last_name+ " my name is bot")
